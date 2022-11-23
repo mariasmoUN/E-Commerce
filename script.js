6ab3fac2d1634e30de8f7888aa35ed7590194976
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2022-11-23 17:42:56 -0500

    Añadiendo funcionalidad al botón de búsqueda

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const navMenu = document.querySelector('.nav-menu');
 const btnMenu = document.querySelector('.btn-menu');
+const btnBuscar = document.querySelector('.search-btn');
+const inputBuscar = document.querySelector('.container-search');
+const cerrarBuscar = document.querySelector('.btn-close');
 
 /* ===== Menu de navegación ===== */
 btnMenu.addEventListener('click', () => {
@@ -14,4 +17,19 @@ btnMenu.addEventListener('click', () => {
         navMenu.classList.remove('active');
         navMenu.classList.add('not-active');
     }
+})
+
+/* ===== Botón de busqueda ===== */
+btnBuscar.addEventListener('click', () => {
+    btnBuscar.classList.remove('not-active');
+    btnBuscar.classList.add('active');
+    inputBuscar.classList.remove('not-active');
+    inputBuscar.classList.add('active'); 
+})
+
+cerrarBuscar.addEventListener('click', () => {
+    btnBuscar.classList.remove('active');
+    btnBuscar.classList.add('not-active');
+    inputBuscar.classList.remove('active');
+    inputBuscar.classList.add('not-active');
 })
